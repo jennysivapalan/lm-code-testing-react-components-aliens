469dcf7b17576ec518be1e23ef4c00783b2c3c82
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-12-13 18:09:28 +0000

    feat: implements validation for planet name and sending error on special characters
    test: Adds tests for more than one error on planet name

--- src/validate/validate_planet_name.tsx
+++ src/validate/validate_planet_name.tsx
@@ -1,10 +1,11 @@
 const LENGTH_ERROR = "Planet Name: Must be between 2 and 49 characters";
+const CHARACTER_ERROR = "Planet Name: No special characters allowed";
 
 const validatePlanetName: (planetName: string) => string[] = (speciesName) => {
   const errorMessages = Array<string>();
   if (speciesName.length < 2 || speciesName.length > 49)
     errorMessages.push(LENGTH_ERROR);
-
+  if (speciesName.match("[^a-zA-Z0-9 ]")) errorMessages.push(CHARACTER_ERROR);
   return errorMessages;
 };
 
