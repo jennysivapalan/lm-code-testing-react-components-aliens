8aff7a3f9a5ee6137ec3384b94e9052780e42a79
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-12-14 16:04:57 +0000

    test: adds validation mock to planet component and ensure it renders the error message when required.

--- src/components/PlanetName.test.tsx
+++ src/components/PlanetName.test.tsx
@@ -1,13 +1,16 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import PlanetName from "./PlanetName";
-import validatePlanetName from "../validate/validate_planet_name";
 
 test("renders form element and displays planetName props", () => {
+  const mockOnChange = jest.fn();
+  const mockValidatePlanetName = jest.fn();
+  mockValidatePlanetName.mockReturnValue([]);
+
   render(
     <PlanetName
       planetName="mars"
-      onChangePlanetName={() => void {}}
-      validateField={validatePlanetName}
+      onChangePlanetName={mockOnChange}
+      validateField={mockValidatePlanetName}
     />
   );
 
@@ -15,20 +18,42 @@ test("renders form element and displays planetName props", () => {
   expect(labelText).toBeInTheDocument();
   const inputText = screen.getByDisplayValue("mars");
   expect(inputText).toBeInTheDocument();
+
+  const errorMessage = screen.queryByText("Planet name: error!panic!");
+  expect(errorMessage).toBe(null);
 });
 
 test("calls the onChange function in input with correct value", () => {
   const mockOnChange = jest.fn();
+  const mockValidatePlanetName = jest.fn();
+  mockValidatePlanetName.mockReturnValue([]);
 
   render(
     <PlanetName
       planetName="mars"
       onChangePlanetName={mockOnChange}
-      validateField={validatePlanetName}
+      validateField={mockValidatePlanetName}
     />
   );
 
   const input = screen.getByRole("textbox");
   fireEvent.change(input, { target: { value: "Earth" } });
   expect(mockOnChange).toHaveBeenCalledWith("Earth");
+});
+
+test("error messages are displayed when invalid planet name is used", () => {
+  const mockOnChange = jest.fn();
+  const mockValidatePlanetName = jest.fn();
+  mockValidatePlanetName.mockReturnValue(["panic!"]);
+
+  render(
+    <PlanetName
+      planetName="mars"
+      onChangePlanetName={mockOnChange}
+      validateField={mockValidatePlanetName}
+    />
+  );
+
+  const errorMessage = screen.getByText("panic!");
+  expect(errorMessage).toBeInTheDocument();
 });
