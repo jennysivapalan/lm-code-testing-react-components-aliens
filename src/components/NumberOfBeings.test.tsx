44596c370bf64b114b72e6403a5c225e57d5ffda
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-12-14 16:09:38 +0000

    test: adds validation mock to number of beings component and ensure it renders the error message when required.

--- src/components/NumberOfBeings.test.tsx
+++ src/components/NumberOfBeings.test.tsx
@@ -1,13 +1,16 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import NumberOfBeings from "./NumberOfBeings";
-import validateNumberOfBeings from "../validate/validate_number_of_beings";
 
 test("renders form element and displays numberOfBeing props", () => {
+  const mockOnChange = jest.fn();
+  const mockValidateNumberOfBeings = jest.fn();
+  mockValidateNumberOfBeings.mockReturnValue([]);
+
   render(
     <NumberOfBeings
       numberOfBeings="2"
-      onChangeBeings={() => void {}}
-      validateField={validateNumberOfBeings}
+      onChangeBeings={mockOnChange}
+      validateField={mockValidateNumberOfBeings}
     />
   );
 
@@ -15,19 +18,41 @@ test("renders form element and displays numberOfBeing props", () => {
   expect(labelText).toBeInTheDocument();
   const inputText = screen.getByDisplayValue("2");
   expect(inputText).toBeInTheDocument();
+  const errorMessage = screen.queryByText("Error message on beings number");
+  expect(errorMessage).toBe(null);
 });
 
 test("calls the onChange function in input with correct value", () => {
   const mockOnChange = jest.fn();
+  const mockValidateNumberOfBeings = jest.fn();
+  mockValidateNumberOfBeings.mockReturnValue([]);
   render(
     <NumberOfBeings
       numberOfBeings="2"
       onChangeBeings={mockOnChange}
-      validateField={validateNumberOfBeings}
+      validateField={mockValidateNumberOfBeings}
     />
   );
 
   const input = screen.getByRole("textbox");
   fireEvent.change(input, { target: { value: "10" } });
   expect(mockOnChange).toHaveBeenCalledWith("10");
+});
+
+test("error messages are displayed when invalid number of beings is used", () => {
+  const mockOnChange = jest.fn();
+  const mockValidateNumberOfBeings = jest.fn();
+  mockValidateNumberOfBeings.mockReturnValue(["Beings should be more than 3"]);
+
+  render(
+    <NumberOfBeings
+      numberOfBeings="2"
+      onChangeBeings={mockOnChange}
+      validateField={mockValidateNumberOfBeings}
+    />
+  );
+
+  const errorMessage = screen.getByText("Beings should be more than 3");
+
+  expect(errorMessage).toBeInTheDocument();
 });
