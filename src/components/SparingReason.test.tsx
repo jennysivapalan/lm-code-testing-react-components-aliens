aa4dd607738210c3111288dbd223b2cbf902b0df
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-12-14 16:14:48 +0000

    test: adds validation mock to sparing reason component and ensure it renders the error message when required.

--- src/components/SparingReason.test.tsx
+++ src/components/SparingReason.test.tsx
@@ -1,12 +1,15 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import SparingReason from "./SparingReason";
-import validateSparingReason from "../validate/validate_sparing_reason";
 test("renders form element and displays sparingReason props", () => {
+  const mockOnChange = jest.fn();
+  const mockValidateSparingReason = jest.fn();
+  mockValidateSparingReason.mockReturnValue([]);
+
   render(
     <SparingReason
       sparingReason="Life is good!"
-      onChangeSparingReason={() => void {}}
-      validateField={validateSparingReason}
+      onChangeSparingReason={mockOnChange}
+      validateField={mockValidateSparingReason}
     />
   );
 
@@ -14,19 +17,47 @@ test("renders form element and displays sparingReason props", () => {
   expect(labelText).toBeInTheDocument();
   const inputText = screen.getByDisplayValue("Life is good!");
   expect(inputText).toBeInTheDocument();
+  const errorMessage = screen.queryByText(
+    "Reason for sparing: Must be between 17 and 153 characters"
+  );
+  expect(errorMessage).toBe(null);
 });
 
 test("calls the onChange function in input with correct value", () => {
   const mockOnChange = jest.fn();
+  const mockValidateSparingReason = jest.fn();
+  mockValidateSparingReason.mockReturnValue([]);
 
   render(
     <SparingReason
       sparingReason="Life is good!"
       onChangeSparingReason={mockOnChange}
-      validateField={validateSparingReason}
+      validateField={mockValidateSparingReason}
     />
   );
   const input = screen.getByRole("textbox");
   fireEvent.change(input, { target: { value: "Yes it is" } });
   expect(mockOnChange).toHaveBeenCalledWith("Yes it is");
+});
+
+test("error messages are displayed when invalid species name is used", () => {
+  const mockOnChange = jest.fn();
+  const mockValidateSparingReason = jest.fn();
+  mockValidateSparingReason.mockReturnValue([
+    "Reason for sparing: Must be between 17 and 153 characters",
+  ]);
+
+  render(
+    <SparingReason
+      sparingReason="Life is good!"
+      onChangeSparingReason={mockOnChange}
+      validateField={mockValidateSparingReason}
+    />
+  );
+
+  const errorMessage = screen.getByText(
+    "Reason for sparing: Must be between 17 and 153 characters"
+  );
+
+  expect(errorMessage).toBeInTheDocument();
 });
